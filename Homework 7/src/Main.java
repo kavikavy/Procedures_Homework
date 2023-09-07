cd6552ce364ccd63b1edb8c34eef9f87bcb7dd24
Kavika Vasanthakumar <kaviv0606@gmail.com>
2023-09-07 15:00:26 +0300

    initial commit

--- Homework 7/src/Main.java
+++ Homework 7/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 1; i < 100; i++) {
+            if (prime(i) == true)
+                System.out.print(i + " ");
+        }
+        System.out.println();
+        int count = 0;
+        for (int i = 1; count < 100; i++) {
+            if (prime(i) == true) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+    }
+
+    static boolean prime (int n) {
+        boolean flag = true;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     }
 }
